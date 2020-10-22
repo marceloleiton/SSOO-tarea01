2d94b65f6bcc3426c50836866156ac58df9ea067
Marcelo Leiton <marcelo.leiton@alumnos.uv.cl>
2020-10-22 20:06:23 -0300

    organización actualizada

--- src/hora.c
+++ src/hora.c
@@ -2,4 +2,22 @@
 #include <stdio.h>
 #include <signal.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 #include <time.h>
+#include <signal.h>
+
+void handler(int signal_num)
+{
+    static uint32_t trials = 0;
+
+    trials++;
+    printf(" Para interrumpir programa (ctrl+c) %s  veces.\n", signal_num);
+
+    if (trials == 2)
+    {
+        printf("\nPrograma finalizado con éxito.\n");
+        exit(signal_num);
+    }
+}
