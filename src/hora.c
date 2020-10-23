f24202b233133fc566d02dd2b00f7bc6a919384f
Marcelo Leiton <marcelo.leiton@alumnos.uv.cl>
2020-10-22 22:11:25 -0300

    leves mejoras en codigo, organización y escritura

--- src/hora.c
+++ src/hora.c
@@ -5,43 +5,37 @@
 #include <stdlib.h>
 #include <time.h>
 #include <signal.h>
-
+//manejo recibo de señal
 void recibidora(int sig){
     time_t tiempoactual;
     time(&tiempoactual);
-    printf("\nSeñal SIGUSR1 recibida: %s\n",ctime(&tiempoactual));
+    if (signal(SIGUSR1, recibidora) == SIG_ERR){
+        perror("no puedo obtener la señal");
+    }
+    else{
+        printf("\nSeñal SIGUSR1 recibida: %s\n", ctime(&tiempoactual));
+    }
 }
 //Manejador de finalización del proceso
-void handler(int signal_num)
-{
+void handler(int signal_num){
     static uint32_t trials = 0;
     trials++;
     printf(" Para interrumpir programa (ctrl+c) %d  veces.\n", signal_num);
-    if (trials == 2)
-    {
+    if (trials == 2){
         printf("\nPrograma finalizado con éxito.\n");
         exit(signal_num);
     }
 }
 int main(){
-    signal(SIGTERM, handler);
-    signal(SIGINT, handler);
-    signal(SIGKILL, handler);
-    signal(SIGUSR1, &recibidora);
-    printf("\nPrograma hora ejecutandose. PID= %d \n", getpid());
     int contador = 0;
-    while (++contador)
-    {
-    //std::this_thread::sleep_for(std::chrono::seconds(1));
-    printf("Listo para recibir señal SIGUSR1.\n");
-    sleep(20);
-    //if (signal(SIGUSR1,recibidora) == SIG_ERR)
-    //{
-        //perror("no puedo obtener la señal");
-    //}
-    //else
-    //{  
-    //}
+    while (++contador){
+        signal(SIGTERM, handler);
+        signal(SIGINT, handler);
+        signal(SIGKILL, handler);
+        signal(SIGUSR1, &recibidora);
+        printf("Programa hora ejecutandose. PID= %d \n", getpid());
+        printf("Listo para recibir señal SIGUSR1.\n");
+        sleep(40);
     }
     return 0;
 }
