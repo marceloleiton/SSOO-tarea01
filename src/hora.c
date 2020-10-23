96c32eb2d0c11cb8455db8802542916fc956cd23
Marcelo Leiton <marcelo.leiton@alumnos.uv.cl>
2020-10-22 21:33:34 -0300

    cambios en funciones (hora)

--- src/hora.c
+++ src/hora.c
@@ -1,15 +1,15 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <signal.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 #include <time.h>
 #include <signal.h>
-void recibidora(int signo){
-    if(signo == SIGUSR1){printf("recibi la señal");}
-    else{printf("error de señal");}   
+
+void recibidora(int sig){
+    time_t tiempoactual;
+    time(&tiempoactual);
+    printf("\nSeñal SIGUSR1 recibida: %s\n",ctime(&tiempoactual));
 }
 //Manejador de finalización del proceso
 void handler(int signal_num)
@@ -28,18 +28,23 @@ int main(){
     signal(SIGTERM, handler);
     signal(SIGINT, handler);
     signal(SIGKILL, handler);
-
-    int contador = 0;  
+    signal(SIGUSR1, &recibidora);
+    int contador = 0;
     while (++contador)
     {
     //std::this_thread::sleep_for(std::chrono::seconds(1));
-    sleep(1);
-    printf("\nPrograma en ejecución ... PID= %d \n", getpid());
-    printf("\nEn espera de señal SIGUSR1");
-    if (contador == 100)
-    {
-        //printf(SIGTERM);
-        }
+    
+    printf("\nPrograma hora ejecutandose. PID= %d \n", getpid());
+    printf("Listo para recibir señal SIGUSR1.\n");
+    sleep(100);
+    //if (signal(SIGUSR1,recibidora) == SIG_ERR)
+    //{
+        //perror("no puedo obtener la señal");
+    //}
+    //else
+    //{
+        
+    //}
     }
     return 0;
 }
