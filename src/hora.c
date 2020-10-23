3598db225bc7ee01c728023fc23bfb51784b89fb
Marcelo Leiton <marcelo.leiton@alumnos.uv.cl>
2020-10-22 23:40:17 -0300

    comentarios, y arreglo en funciones (nombres)

--- src/hora.c
+++ src/hora.c
@@ -1,20 +1,27 @@
+//Declaración de librerías utilizadas.
 #include <signal.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-//manejo recibo de señal
-void recibidora(int sig){
+//Recibed_Signal -> función que recibe la señal de SIGUSR1 y imprime el tiempo al recibirla.
+//Se ingresa por argumento un entero, el número de la señal SIGUSR1.
+void Recibed_Signal(int sig){
     time_t tiempoactual;
     time(&tiempoactual);
-    if (signal(SIGUSR1, recibidora) == SIG_ERR){
+    //Sentencias condicionales indican si recibe la señal o no, y que hacer en ambos casos.
+    if (signal(SIGUSR1, Recibed_Signal) == SIG_ERR){
         perror("Error al obtener la señal");
     }
     else{
-        printf("\nSeñal SIGUSR1 recibida: %s\n", ctime(&tiempoactual));
+        if (tiempoactual == -1){
+            printf("Error al encontrar el tiempo");
+        }else{
+            printf("\nSeñal SIGUSR1 recibida: %s\n", ctime(&tiempoactual));
+        }
     }
 }
-//Manejador de finalización del proceso
+//Manejador para el control de termino de ejecución, para que cumpla solo la condición de (ctrl+c) 2 veces.
 void handler(int signal_num){
     static uint32_t trials = 0;
     trials++;
@@ -22,19 +29,20 @@ void handler(int signal_num){
     if (trials == 2){
         printf("\nPrograma finalizado con éxito.\n");
         exit(signal_num);
-    
     }
 }
 int main(){
+    //Manejo de señales para el control de termino de ejecución.
+    signal(SIGTERM, handler);
+    signal(SIGINT, handler);
+    signal(SIGKILL, handler);
+    //se recibe la señal (&Recibed_Signal), cuando el proceso reciba la interrupción SIGUSR1.
+    signal(SIGUSR1, &Recibed_Signal);
     int contador = 0;
+    printf("Programa hora ejecutandose. PID= %d \n", getpid());
     while (++contador){
-        signal(SIGTERM, handler);
-        signal(SIGINT, handler);
-        signal(SIGKILL, handler);
-        signal(SIGUSR1, &recibidora);
-        printf("Programa hora ejecutandose. PID= %d \n", getpid());
         printf("Listo para recibir señal SIGUSR1.\n");
-        sleep(40);
+        sleep(1000);
     }
     return 0;
 }
